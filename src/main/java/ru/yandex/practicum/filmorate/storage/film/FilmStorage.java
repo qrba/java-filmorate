f656919087b29c603d0f0259645c1f2dd9ebd706
Алексей Курбатский <akurbatskiy@mwnts.ru>
2023-07-29 22:53:17 +0300

    refactor: codestyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,7 +6,10 @@ import java.util.Collection;
 
 public interface FilmStorage {
     Collection<Film> getAll();
+
     Film add(Film film);
+
     Film update(Film film);
+
     Film getFilmById(int id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,7 +6,10 @@ import java.util.Collection;
 
 public interface UserStorage {
     Collection<User> getAll();
+
     User add(User user);
+
     User update(User user);
+
     User getUserById(int id);
 }
